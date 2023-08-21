dc9eefd59fe1b278b7fe57c4d12ef8997eba5a1c
vilsonpneto <vilson@kenzie.com.br>
2023-08-21 11:28:29 -0300

    Add file

---
+++ Sprint 4/01 - Introdução ao Javascript - Manha/script.js
@@ -0,0 +1,76 @@
+console.log("Olá mundo!");
+console.log("Thiago");
+
+// Atribuimos um valor a uma variável
+// let é uma variavel mutável, pode mudar
+let nome = "Lorena";
+console.log(nome);
+
+// Reatribuimos de valor
+nome = "Alex";
+console.log(nome);
+
+// Camel Case: dataNascimento -> Boa prática JS
+// Snake Case: data-nascimento
+
+// const é uma variavel constante, não pode mudar
+const dataNascimento = "05/07/1750";
+console.log(dataNascimento);
+
+// NÃO UTILIZAMOS MAIS O VAR
+//var idade = 20;
+//idade = 20;
+//console.log(idade);
+
+
+// Tipos de dados primitivos
+// integer (Number) -> Número inteiro
+const numero = 10;
+console.log(numero);
+
+// float (Number) -> Número flutuante
+const fluido = 10.5;
+console.log(fluido);
+
+// String -> Texto
+const texto = "JS é muito bom!";
+console.log(texto);
+
+// Boleano (boolean) -> Verdadeiro/Falso
+const verdadeiro = true;
+console.log(verdadeiro);
+const falso = false;
+console.log(falso);
+
+let comChave = false;
+
+// Fiz uma ação de pegar a chave
+
+comChave = true;
+
+// Mudanças de tipo de dado em uma variável
+let batata = 10;
+console.log(batata);
+
+batata = "10";
+console.log(batata);
+
+batata = false;
+console.log(batata);
+
+let nomeUsuario = "Julia";
+
+const pi = 3.1415;
+
+// Operadores:
+
+//adição (+)
+// 10 + 10
+//subtração (-)
+// 15 - 5
+//divisão (/)
+// 10 / 5
+//multiplicação (*)
+//5 * 5
+//potenciação (**)
+// 3 ** 2
