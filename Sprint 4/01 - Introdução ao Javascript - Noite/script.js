33e8309220fbb15ff62c2fde3606b65799b4060c
thiagoKenzie <102174562+thiagoKenzie@users.noreply.github.com>
2023-08-22 10:34:36 -0300

    Add files via upload

---
+++ Sprint 4/01 - Introdução ao Javascript - Noite/script.js
@@ -0,0 +1,63 @@
+console.log("Kenzie Academy Brasil");
+console.log('Thiago Koman Schmidt');
+console.log(22);
+
+/* alert('João Legal'); */
+
+// Comentar automaticamente -> CTRL + ;
+
+// VARIAVEIS
+let nome = 'Thiago';
+console.log(nome);
+
+nome = 'Yan';
+console.log(nome);
+
+
+const idade = 22;
+console.log(idade);
+
+// Não deu certo!
+// idade = 23;
+// console.log(idade);
+
+
+const number = 67;
+const float = 1.5;
+const string = 'texto'; //"texto"
+const boolean = true; //false
+
+
+// OPERADORES MATEMATICOS
+const a = 12;
+const b = 3;
+const c = 4;
+
+let result = a+b; // 12 + 3 = 15
+console.log(result);
+
+result = a-b; // 12 - 3 = 9
+console.log(result);
+
+result = b*c; // 3 * 4 = 12
+console.log(result);
+
+result = a/b; // 12 / 3 = 4
+console.log(result);
+
+
+// STRINGS
+let str = "Kenzie Academy"; //14
+let tamanho = str.length;
+console.log(tamanho);
+console.log(str);
+
+str = str + " Brasil"; // 14 + 7 = 21
+tamanho = str.length;
+console.log(tamanho);
+console.log(str);
+
+str = 'Escola de programação ' + str; // 22 + 21 = 43
+tamanho = str.length;
+console.log(tamanho);
+console.log(str);
